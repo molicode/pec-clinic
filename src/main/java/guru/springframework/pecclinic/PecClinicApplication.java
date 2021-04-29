b2e1fb4c0f6d1ad948c1a92bd7c42934b11a1dee
Luis Molina <molicode1347@gmail.com>
2021-04-29 18:12:05 -0300

    Initial Project Add

---
+++ src/main/java/guru/springframework/pecclinic/PecClinicApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.pecclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PecClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PecClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/guru/springframework/pecclinic/PecClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.pecclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PecClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
